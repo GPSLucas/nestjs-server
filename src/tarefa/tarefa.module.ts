8b01b265a1e9e3e60a5fb9e6947eca97f5f4db70
LucasGps <gp.lucas04@gmail.com>
2025-04-30 13:20:59 -0300

    atualizando prisma

--- src/tarefa/tarefa.module.ts
+++ src/tarefa/tarefa.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TarefaService } from './tarefa.service';
 import { TarefaController } from './tarefa.controller';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [TarefaController],
--- src/tarefa/tarefa.service.ts
+++ src/tarefa/tarefa.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient, Tarefa } from '@prisma/client';
+import { Tarefa } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateTarefaDto } from './dto/create-tarefa.dto';
-
-const prisma = new PrismaClient();
 
 @Injectable()
 export class TarefaService {
+  constructor(private readonly prisma: PrismaService) {}
+
   async findAll(): Promise<Tarefa[]> {
-    return prisma.tarefa.findMany({ orderBy: { created_at: 'desc' } });
+    return this.prisma.tarefa.findMany({ orderBy: { created_at: 'desc' } });
   }
 
   async create(data: CreateTarefaDto): Promise<Tarefa> {
-    return prisma.tarefa.create({ data });
+    return this.prisma.tarefa.create({ data });
   }
 
   async delete(id: number): Promise<Tarefa> {
-    return prisma.tarefa.delete({ where: { id } });
+    return this.prisma.tarefa.delete({ where: { id } });
   }
 }
